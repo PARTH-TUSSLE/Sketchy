6a014f262aab0e9f44bc985a315cf3356397ee07
PARTH-TUSSLE <parthgartan26feb@gmail.com>
2025-10-05 13:32:26 +0530

    Added shape history

--- apps/sketchy-frontend/app/canvas/[roomId]/page.tsx
+++ apps/sketchy-frontend/app/canvas/[roomId]/page.tsx
@@ -1,37 +1,17 @@
 "use client";
 
 import { useEffect, useRef } from "react";
+import initDraw from "../../draw/index"
 
 export default function Canvas() {
   const canvasRef = useRef<HTMLCanvasElement>(null);
 
   useEffect(() => {
-    const canvas = canvasRef.current;
-    const ctx = canvas?.getContext("2d");
 
-    let clicked = false;
-    let startX = 0;
-    let startY = 0;
-
-    canvas?.addEventListener("mousedown", (e) => {
-      clicked = true;
-      startX = e.clientX;
-      startY = e.clientY;
-    });
-
-    canvas?.addEventListener("mouseup", (e) => {
-      clicked = false;
-    });
-
-    canvas?.addEventListener("mousemove", (e) => {
-      let width = e.clientX - startX;
-      let height = e.clientY - startY;
-
-      if (clicked) {
-        ctx?.clearRect(0, 0, canvas.width, canvas.height);
-        ctx?.strokeRect(startX, startY, width, height);
-      }
-    });
+    if ( canvasRef.current ){
+    initDraw(canvasRef.current);
+    }
+    
   }, [canvasRef]);
 
   return (
@@ -39,9 +19,16 @@ export default function Canvas() {
       <canvas
         className="bg-neutral-300 "
         ref={canvasRef}
-        width={1000}
-        height={1000}
+        width={1439}
+        height={810}
       ></canvas>
+      <div
+        className="absolute top-0 left-0 bg-neutral-300
+      text-black"
+      >
+        <button>rectangle</button> &nbsp; &nbsp;
+        <button>circle</button>
+      </div>
     </div>
   );
 }
